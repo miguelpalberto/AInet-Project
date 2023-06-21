5df232d10ca1f8c60021aef24b18328918070cf8
José Pedro Martins <102488371+josemartins125@users.noreply.github.com>
2023-06-21 22:17:20 +0100

    Imagens Clientes
    Imagens Clientes Feito

--- resources/views/customers/shared/table.blade.php
+++ resources/views/customers/shared/table.blade.php
@@ -2,7 +2,7 @@
     <thead class="table-dark">
         <tr>
             @if ($showFoto)
-                <th></th>
+            <th></th>
             @endif
             <th>Nome</th>
             <th>E-Mail</th>
@@ -12,76 +12,65 @@
             <th>Tipo Pagamento</th>
             <th>Referência Pagamento</th>
             @if ($showDates)
-                <th>Data Criação</th>
-                <th>Data Edição</th>
-                <th>Data Remoção</th>
+            <th>Data Criação</th>
+            <th>Data Edição</th>
+            <th>Data Remoção</th>
             @endif
             @if ($showDetail)
-                <th class="button-icon-col"></th>
+            <th class="button-icon-col"></th>
             @endif
             @if ($showEdit)
-                <th class="button-icon-col"></th>
+            <th class="button-icon-col"></th>
             @endif
             @if ($showDelete)
-                <th class="button-icon-col"></th>
+            <th class="button-icon-col"></th>
             @endif
         </tr>
     </thead>
     <tbody>
         @foreach ($customers as $customer)
-            <tr>
-                @if ($showFoto)
-                {{-- TODO --}}
-                    <td><img src="/img/avatar_unknown.png" alt="Avatar" class="bg-dark rounded-circle" width="45"
-                            height="45"></td>
+        <tr>
+            @if ($showFoto)
+            <td width="45">
+                @if ($customer->user->photo_url)
+                <img src="{{ $customer->user->fullPhotoUrl }}" alt="Avatar" class="bg-dark rounded-circle" width="45" height="45">
                 @endif
-                <td>{{ $customer->user->name }}</td>
-                <td>{{ $customer->user->email }}</td>
+            </td>
+            @endif
+            <td>{{ $customer->user->name }}</td>
+            <td>{{ $customer->user->email }}</td>
 
-                <td>{{ $customer->nif }}</td>
-                <td>{{ $customer->address }}</td>
-                <td>{{ $customer->default_payment_type }}</td>
-                <td>{{ $customer->default_payment_ref }}</td>
+            <td>{{ $customer->nif }}</td>
+            <td>{{ $customer->address }}</td>
+            <td>{{ $customer->default_payment_type }}</td>
+            <td>{{ $customer->default_payment_ref }}</td>
 
-                {{-- TODO - show cenas é diferente ficha 9--}}
-                @if ($showDates)
-                    <td>{{ $customer->created_at }}</td>
-                    <td>{{ $customer->updated_at }}</td>
-                    <td>{{ $customer->deleted_at }}</td>
-                @endif
+            {{-- TODO - show cenas é diferente ficha 9--}}
+            @if ($showDates)
+            <td>{{ $customer->created_at }}</td>
+            <td>{{ $customer->updated_at }}</td>
+            <td>{{ $customer->deleted_at }}</td>
+            @endif
 
-                @if ($showDetail)
-                    <td class="button-icon-col"><a class="btn btn-secondary"
-                            href="{{ route('customers.show', ['customer' => $customer]) }}">
-                            <i class="fas fa-eye"></i></a></td>
-                @endif
-                @if ($showEdit)
-                    <td class="button-icon-col"><a class="btn btn-dark"
-                            href="{{ route('customers.edit', ['customer' => $customer]) }}">
-                            <i class="fas fa-edit"></i></a></td>
-                @endif
-                @if ($showDelete)
-                    <td class="button-icon-col">
-                        <form method="POST" action="{{ route('customers.destroy', ['customer' => $customer]) }}">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" name="delete" class="btn btn-danger">
-                                <i class="fas fa-trash"></i></button>
-                        </form>
-                    </td>
-                @endif
-            </tr>
+            @if ($showDetail)
+            <td class="button-icon-col"><a class="btn btn-secondary" href="{{ route('customers.show', ['customer' => $customer]) }}">
+                    <i class="fas fa-eye"></i></a></td>
+            @endif
+            @if ($showEdit)
+            <td class="button-icon-col"><a class="btn btn-dark" href="{{ route('customers.edit', ['customer' => $customer]) }}">
+                    <i class="fas fa-edit"></i></a></td>
+            @endif
+            @if ($showDelete)
+            <td class="button-icon-col">
+                <form method="POST" action="{{ route('customers.destroy', ['customer' => $customer]) }}">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" name="delete" class="btn btn-danger">
+                        <i class="fas fa-trash"></i></button>
+                </form>
+            </td>
+            @endif
+        </tr>
         @endforeach
     </tbody>
 </table>
-
-
-
-
-
-
-
-
-
-
-
--- resources/views/users/shared/table.blade.php
+++ resources/views/users/shared/table.blade.php
@@ -2,7 +2,7 @@
     <thead class="table-dark">
         <tr>
             @if ($showFoto)
-                <th></th>
+            <th></th>
             @endif
             <th>Nome</th>
             <th>Email</th>
@@ -10,73 +10,74 @@
             <th>Bloqueado</th>
             <th>Foto</th>
             @if ($showDates)
-                <th>Data Criação</th>
-                <th>Data Edição</th>
-                <th>Data Remoção</th>
+            <th>Data Criação</th>
+            <th>Data Edição</th>
+            <th>Data Remoção</th>
             @endif
             @if ($showDetail)
-                <th class="button-icon-col"></th>
+            <th class="button-icon-col"></th>
             @endif
             @if ($showEdit)
-                <th class="button-icon-col"></th>
-                <th class="button-icon-col"></th>
+            <th class="button-icon-col"></th>
+            <th class="button-icon-col"></th>
             @endif
             @if ($showDelete)
-                <th class="button-icon-col"></th>
+            <th class="button-icon-col"></th>
             @endif
         </tr>
     </thead>
     <tbody>
         @foreach ($users as $user)
-            <tr>
-                @if ($showFoto)
-                    {{-- TODO --}}
-                    <td><img src="/img/avatar_unknown.png" alt="Avatar" class="bg-dark rounded-circle" width="45"
-                            height="45"></td>
+        <tr>
+            @if ($showFoto)
+            <td width="45">
+                @if ($user->photo_url)
+                <img src="{{ $user->fullPhotoUrl }}" alt="Avatar" class="bg-dark rounded-circle" width="45" height="45">
                 @endif
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->email }}</td>
-                <td>{{ $user->user_type }}</td>
-                <td>{{ $user->blocked }}</td>
-                <td>{{ $user->photo_url }}</td>
+            </td>
 
-                {{-- TODO - show cenas é diferente ficha 9 --}}
-                @if ($showDates)
-                    <td>{{ $user->created_at }}</td>
-                    <td>{{ $user->updated_at }}</td>
-                    <td>{{ $user->deleted_at }}</td>
-                @endif
+            @endif
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            <td>{{ $user->user_type }}</td>
+            <td>{{ $user->blocked }}</td>
+            <td>{{ $user->photo_url }}</td>
 
-                @if ($showDetail)
-                    <td class="button-icon-col"><a class="btn btn-secondary"
-                            href="{{ route('users.show', ['user' => $user]) }}">
-                            <i class="fas fa-eye"></i></a></td>
-                @endif
-                @if ($showEdit)
-                    <td class="button-icon-col"><a class="btn btn-dark"
-                            href="{{ route('users.edit', ['user' => $user]) }}">
-                            <i class="fas fa-edit"></i></a></td>
-                    {{-- bloquear: --}}
-                    <td class="button-icon-col">
-                        <form method="POST" action="{{ route('usersBlock', ['user' => $user]) }}">
-                            @csrf
-                            @method('PATCH')
-                            <button type="submit" name="usersBlock" class="btn btn-dark">
-                                <i class="fas fa-unlock"></i></button>
-                        </form>
-                    </td>
-                @endif
-                @if ($showDelete)
-                    <td class="button-icon-col">
-                        <form method="POST" action="{{ route('users.destroy', ['user' => $user]) }}">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" name="delete" class="btn btn-danger">
-                                <i class="fas fa-trash"></i></button>
-                        </form>
-                    </td>
-                @endif
-            </tr>
+            {{-- TODO - show cenas é diferente ficha 9 --}}
+            @if ($showDates)
+            <td>{{ $user->created_at }}</td>
+            <td>{{ $user->updated_at }}</td>
+            <td>{{ $user->deleted_at }}</td>
+            @endif
+
+            @if ($showDetail)
+            <td class="button-icon-col"><a class="btn btn-secondary" href="{{ route('users.show', ['user' => $user]) }}">
+                    <i class="fas fa-eye"></i></a></td>
+            @endif
+            @if ($showEdit)
+            <td class="button-icon-col"><a class="btn btn-dark" href="{{ route('users.edit', ['user' => $user]) }}">
+                    <i class="fas fa-edit"></i></a></td>
+            {{-- bloquear: --}}
+            <td class="button-icon-col">
+                <form method="POST" action="{{ route('usersBlock', ['user' => $user]) }}">
+                    @csrf
+                    @method('PATCH')
+                    <button type="submit" name="usersBlock" class="btn btn-dark">
+                        <i class="fas fa-unlock"></i></button>
+                </form>
+            </td>
+            @endif
+            @if ($showDelete)
+            <td class="button-icon-col">
+                <form method="POST" action="{{ route('users.destroy', ['user' => $user]) }}">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" name="delete" class="btn btn-danger">
+                        <i class="fas fa-trash"></i></button>
+                </form>
+            </td>
+            @endif
+        </tr>
         @endforeach
     </tbody>
 </table>
