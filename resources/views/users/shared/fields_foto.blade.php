d63a2e5c9814566b3fa8c44238bef6d6ee385b70
José Pedro Martins <102488371+josemartins125@users.noreply.github.com>
2023-06-22 10:08:26 +0100

    Update fields_foto.blade.php

--- resources/views/users/shared/fields_foto.blade.php
+++ resources/views/users/shared/fields_foto.blade.php
@@ -1,11 +1,8 @@
-
-<!-- Ver com o Prof -->
 <img src="{{ $user->fullPhotoUrl }}" alt="Avatar" class="rounded-circle img-thumbnail">
 
 @if ($allowUpload)
     <div class="mb-3 pt-3">
-        <input type="file" class="form-control @error('file_foto') is-invalid @enderror" name="file_foto"
-            id="inputFileFoto">
+        <input type="file" class="form-control" name="file_foto" id="inputFileFoto" @error('file_foto') is-invalid @enderror>
         @error('file_foto')
             <div class="invalid-feedback">
                 {{ $message }}
@@ -13,11 +10,10 @@
         @enderror
     </div>
 @endif
+
 @if (($allowDelete ?? false) && $user->url_foto)
     @if ($user)
-        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#confirmationModal"
-            data-action="{{ route('users.foto.destroy', ['user' => $user]) }}"
-            data-msgLine2="Quer mesmo apagar a fotografia do utilizador <strong>{{ $user->name }}</strong>?">
+        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#confirmationModal" data-action="{{ route('users.foto.destroy', ['user' => $user]) }}" data-msgLine2="Quer mesmo apagar a fotografia do utilizador <strong>{{ $user->name }}</strong>?">
             Apagar Foto
         </button>
     @endif
