f68391e930ec13ff3ccd2ce9ca6e1a19b16b2d1b
José Pedro Martins <102488371+josemartins125@users.noreply.github.com>
2023-06-24 15:24:44 +0100

    Quase Feito o button do Funcionario
    quase quase

--- resources/views/orders/shared/table.blade.php
+++ resources/views/orders/shared/table.blade.php
@@ -19,42 +19,58 @@
     </thead>
     <tbody>
         @foreach ($orders as $order)
-            <tr>
-                <td>{{ $order->id }}</td>
-                <td>{{ $order->status }}</td>
-                <td>{{ $order->customer_id }}</td>
-                <td>{{ $order->date }}</td>
-                <td>{{ $order->total_price }}</td>
-                <td>{{ $order->notes }}</td>
-                <td>{{ $order->nif }}</td>
-                <td>{{ $order->address }}</td>
-                <td>{{ $order->payment_type }}</td>
-                <td>{{ $order->payment_ref }}</td>
-                <td>{{ $order->receipt_url }}</td>
+        <tr>
+            <td>{{ $order->id }}</td>
+            <td>{{ $order->status }}</td>
+            <td>{{ $order->customer_id }}</td>
+            <td>{{ $order->date }}</td>
+            <td>{{ $order->total_price }}</td>
+            <td>{{ $order->notes }}</td>
+            <td>{{ $order->nif }}</td>
+            <td>{{ $order->address }}</td>
+            <td>{{ $order->payment_type }}</td>
+            <td>{{ $order->payment_ref }}</td>
+            <td>{{ $order->receipt_url }}</td>
 
 
-                <td class="button-icon-col"><a class="btn btn-secondary"
-                        href="{{ route('orders.show', ['order' => $order]) }}">
-                        <i class="fas fa-eye"></i></a></td>
-                <td class="button-icon-col"><a class="btn btn-dark"
-                        href="{{ route('orders.edit', ['order' => $order]) }}">
-                        <i class="fas fa-edit"></i></a></td>
-                <td class="button-icon-col"><a class="btn btn-dark"
-                        href="{{ route('orders.edit', ['order' => $order]) }}">
-                        <i class="fas fa-money-check-dollar"></i></a></td>
-                <td class="button-icon-col"><a class="btn btn-dark"
-                        href="{{ route('orders.edit', ['order' => $order]) }}">
-                        <i class="fas fa-clipboard-check"></i></a></td>
+            <td class="button-icon-col"><a class="btn btn-secondary" href="{{ route('orders.show', ['order' => $order]) }}">
+                    <i class="fas fa-eye"></i></a></td>
+            <td class="button-icon-col"><a class="btn btn-dark" href="{{ route('orders.edit', ['order' => $order]) }}">
+                    <i class="fas fa-edit"></i></a></td>
 
-                {{-- <td class="button-icon-col">
-                <form method="POST" action="{{ route('orders.destroy', ['order' => $order]) }}">
+
+            <td class="button-icon-col">
+
+                <form method="POST" action="{{ route('ordersPaid', ['order' => $order]) }}">
                     @csrf
-                    @method('DELETE')
-                    <button type="submit" name="delete" class="btn btn-danger">
-                        <i class="fas fa-trash"></i></button>
+                    @method('PATCH')
+                    <button type="submit" name="ordersPaid" class="btn btn-dark">
+                        <i class="fas fa-money-check-dollar"></i></button>
                 </form>
+            </td>
+
+
+            <td class="button-icon-col">
+
+                <form method="POST" action="{{ route('ordersClosed', ['order' => $order]) }}">
+                    @csrf
+                    @method('PATCH')
+                    <button type="submit" name="ordersClosed" class="btn btn-dark">
+                        <i class="fas fa-clipboard-check"></i></button>
+                </form>
+            </td>
+
+
+
+            {{-- <td class="button-icon-col">
+                <form method="POST" action="{{ route('orders.destroy', ['order' => $order]) }}">
+            @csrf
+            @method('DELETE')
+            <button type="submit" name="delete" class="btn btn-danger">
+                <i class="fas fa-trash"></i></button>
+            </form>
             </td> --}}
-            </tr>
+        </tr>
         @endforeach
     </tbody>
 </table>
